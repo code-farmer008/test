a2d3aa79af4a84eab8d5968ed4b3c0955985ad8c
code-farmer008 <unique0731*&01>
2020-03-30 12:18:58 +0800

    ff

--- util.js
+++ util.js
@@ -1,4 +1,4 @@
-export function getRedirectPath({type, avatar}){
+export function getRedirectPath90({type, avatar}){
 	// 根据用户信息 返回跳转地址
 	// user.type /boss /genius
 	// user.avatar /bossinfo /geniusinfo 
